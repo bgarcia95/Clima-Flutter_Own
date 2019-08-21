e38e5b9fd9f690ae10744be5b8dc2ea328152902
bgarcia95 <bgarcia.95@outlook.com>
2019-08-21 15:01:44 -0600

    Adding spinkit dependecy and reformatting code on loading screen.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+void getData() async {
+  Location location = Location();
+  await location.getLocation();
+
+}
+
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+      body: ,
     );
   }
 }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,2 +1,23 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+    try {
+      if (position != null) {
+        latitude = position.latitude;
+        longitude = position.longitude;
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    print(position);
+  }
+}
